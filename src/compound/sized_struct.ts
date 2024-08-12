22f243440db0f071a5ac668af5ba8a9df3a1f149
mommysgoodpuppy <theelectronicfreezer@gmail.com>
2024-08-12 11:42:59 +0300

    C type stuff

--- src/compound/sized_struct.ts
+++ src/compound/sized_struct.ts
@@ -1,5 +1,5 @@
 import { type InnerType, type Options, SizedType } from "../mod.ts";
-import { calculateTotalSize, getBiggestAlignment } from "../util.ts";
+import { calculateFieldOffsets, calculateStructSize, getBiggestAlignment } from "../util.ts";
 
 type ReadFn<R> = (dt: DataView, options: Options) => R;
 type WriteFn<V> = (dt: DataView, options: Options, value: V) => void;
@@ -13,22 +13,27 @@ const createWrite = (key: string, method: string) =>
 function createFunc<V, M extends `read${string}`>(
   input: Record<string, SizedType<unknown>>,
   method: M,
+  fieldOffsets: Record<string, number>
 ): ReadFn<V>;
 function createFunc<V, M extends `write${string}`>(
   input: Record<string, SizedType<unknown>>,
   method: M,
+  fieldOffsets: Record<string, number>
 ): WriteFn<V>;
 function createFunc<V>(
   input: Record<string, SizedType<unknown>>,
   method: string,
+  fieldOffsets: Record<string, number>
 ): WriteFn<V> | ReadFn<V> {
   const isWriter = method.startsWith("write");
-  const seperator = !isWriter ? "," : "";
+  const separator = !isWriter ? "," : "";
   const keys = Object.keys(input);
 
-  const mapFn = isWriter ? createWrite : createRead;
+  const mapFn = isWriter
+    ? (k: string) => `${k}.${method}(value.${k}, dt, { ...options, byteOffset: options.byteOffset + ${fieldOffsets[k]} });`
+    : (k: string) => `"${k}": ${k}.${method}(dt, { ...options, byteOffset: options.byteOffset + ${fieldOffsets[k]} })`;
 
-  const generatedCodec = keys.map((k) => mapFn(k, method)).join(seperator);
+  const generatedCodec = keys.map(mapFn).join(separator);
   const args = ["dt", "options"];
   let body = `const { ${keys} } = this;`;
 
@@ -53,13 +58,38 @@ export class SizedStruct<
   #read: ReadFn<V>;
   #writePacked: WriteFn<V>;
   #write: WriteFn<V>;
+  #fieldOffsets: Record<string, number>;
+  #fields: T;
 
   constructor(input: T) {
-    super(calculateTotalSize(input), getBiggestAlignment(input));
-    this.#readPacked = createFunc(input, "readPacked");
-    this.#read = createFunc(input, "read");
-    this.#writePacked = createFunc(input, "writePacked");
-    this.#write = createFunc(input, "write");
+    const structSize = calculateStructSize(input);
+    const structAlignment = getBiggestAlignment(input);
+    super(structSize, structAlignment);
+    
+    this.#fieldOffsets = calculateFieldOffsets(input);
+    this.#fields = input;
+    
+    this.#readPacked = createFunc(input, "readPacked", this.#fieldOffsets);
+    this.#read = createFunc(input, "read", this.#fieldOffsets);
+    this.#writePacked = createFunc(input, "writePacked", this.#fieldOffsets);
+    this.#write = createFunc(input, "write", this.#fieldOffsets);
+  }
+
+  getFieldOffsets(prefix: string = ''): Record<string, number> {
+    const result: Record<string, number> = {};
+    for (const [key, field] of Object.entries(this.#fields)) {
+      const fullKey = prefix ? `${prefix}.${key}` : key;
+      const offset = this.#fieldOffsets[key];
+      result[fullKey] = offset;
+      
+      if (field instanceof SizedStruct) {
+        const nestedOffsets = field.getFieldOffsets(fullKey);
+        for (const [nestedKey, nestedOffset] of Object.entries(nestedOffsets)) {
+          result[nestedKey] = offset + nestedOffset;
+        }
+      }
+    }
+    return result;
   }
 
   readPacked(dt: DataView, options: Options = { byteOffset: 0 }): V {
--- src/util.ts
+++ src/util.ts
@@ -9,6 +9,34 @@ export const isLittleEndian: boolean = (() => {
   return new Uint16Array(buffer)[0] === 256;
 })();
 
+export function calculateStructSize(input: Record<string, SizedType<unknown>>): number {
+  let currentOffset = 0;
+  const structAlignment = getBiggestAlignment(input);
+
+  for (const field of Object.values(input)) {
+    // Align the current offset to the field's alignment
+    currentOffset = align(currentOffset, field.byteAlignment);
+    // Add the field's size
+    currentOffset += field.byteSize;
+  }
+
+  // Align the final size to the struct's overall alignment
+  return align(currentOffset, structAlignment);
+}
+
+export function calculateFieldOffsets(input: Record<string, SizedType<unknown>>): Record<string, number> {
+  let currentOffset = 0;
+  const offsets: Record<string, number> = {};
+
+  for (const [key, field] of Object.entries(input)) {
+    currentOffset = align(currentOffset, field.byteAlignment);
+    offsets[key] = currentOffset;
+    currentOffset += field.byteSize;
+  }
+
+  return offsets;
+}
+
 /** Align the value `unaligned` to the first integer that is divisible by `alignment` */
 export const align = (unaligned: number, alignment: number): number =>
   (unaligned + alignment - 1) & ~(alignment - 1);
